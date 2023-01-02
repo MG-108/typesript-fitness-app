55d65584654c23763eb7c3a4e39c2ee23d956980
Matheus Ghignatti <matheusghignatti@gmail.com>
2023-01-02 19:27:00 -0300

    add 'all the navbar implementation for desktop and mobile devices'

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "@/scenes/navbar";
+import { SelectedPage } from "@/shared/types";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleSroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleSroll);
+    return () => window.removeEventListener("scrool", handleSroll);
+  }, []);
+
   return (
     <div className="app bg-gray-20">
-      <Navbar />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 }
