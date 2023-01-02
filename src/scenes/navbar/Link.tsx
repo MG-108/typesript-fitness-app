0933697ce8f5e43c391a66c1be7d237524ca45df
Matheus Ghignatti <matheusghignatti@gmail.com>
2023-01-02 01:49:39 -0300

    first commit

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,25 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+type Props = {
+  page: string;
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
+};
+
+const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
+  // anytime you have a space will remove it
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "");
+  return (
+    <AnchorLink
+      href={`#${lowerCasePage}`}
+      onClick={}
+      className={`${
+        selectedPage === lowerCasePage ? "text-primary-500" : ""
+      } transition duration-500 hover:text-primary-300`}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+export default Link;
