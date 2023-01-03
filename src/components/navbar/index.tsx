4668bceec980df423639195e923cb8c8ffdbdacf
Matheus Ghignatti <matheusghignatti@gmail.com>
2023-01-03 20:25:40 -0300

    add Homepage part1

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -14,7 +14,7 @@ type Props = {
 
 const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
-  const isAboveMediumScreens = useMediaQuery("(min-width:1060px");
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
   const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
